9ac816b748a361eb4b905a88ebd12bad3ef0b843
João Raposo <joaoraposo@optiply.nl>
2025-10-24 11:26:02 +0100

    Initial

---
+++ target_vendit/__init__.py
@@ -0,0 +1,3 @@
+"""Target Vendit - Singer target for Vendit API."""
+
+__version__ = "0.1.0"
---
+++ target_vendit/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for target-vendit."""
+
+from target_vendit.target import TargetVendit
+
+if __name__ == "__main__":
+    TargetVendit.cli()
